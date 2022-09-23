54d2d0f9735f110f222c9da9ad212f7ffcaae17b
Paulo Alenquer <pauloalenquer@gmail.com>
2022-09-23 12:22:09 -0300

    added validate rules to changeset

--- lib/my_recipes/recipe.ex
+++ lib/my_recipes/recipe.ex
@@ -17,5 +17,10 @@ defmodule MyRecipes.Recipe do
    recipe
    |> cast(params, @required_params)
    |> validate_required(@required_params)
+   |> validate_length(:title, min: 4, max: 30)
+   |> validate_length(:ingredients, min: 2, max: 20)
+   |> validate_length(:author, min: 2, max: 20)
+   |> validate_length(:method, min: 10, max: 300)
+   |> validate_inclusion(:cook_time, 1..320)
   end
  end
