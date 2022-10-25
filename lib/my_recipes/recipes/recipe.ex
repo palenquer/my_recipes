96097c49905f320baec0af770c3b0e98581eae43
Paulo Alenquer <pauloalenquer@gmail.com>
2022-10-25 16:57:49 -0300

    added recipe and user schemas

---
+++ lib/my_recipes/recipes/recipe.ex
@@ -0,0 +1,29 @@
+defmodule MyRecipes.Recipe do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias MyRecipes.User
+
+  schema "recipe" do
+    field :title, :string
+    field :author, :string
+    field :method, :string
+    field :cook_time, :integer
+    field :ingredients, {:array, :string}
+    belongs_to :user, User, type: Ecto.UUID
+    timestamps()
+  end
+
+  @required_params [:title, :author, :method, :cook_time, :ingredients, :user_id]
+
+  def changeset(recipe, params) do
+   recipe
+   |> cast(params, @required_params)
+   |> validate_required(@required_params)
+   |> validate_length(:title, min: 4, max: 30)
+   |> validate_length(:ingredients, min: 2, max: 20)
+   |> validate_length(:author, min: 2, max: 20)
+   |> validate_length(:method, min: 10, max: 300)
+   |> validate_inclusion(:cook_time, 1..320)
+  end
+ end
