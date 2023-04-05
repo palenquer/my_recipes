374094a464e9f888cb0e32d69dbea2c5b014a2f1
Paulo Alenquer <pauloalenquer@gmail.com>
2023-04-05 16:03:07 -0300

    user_controller tests

--- test/my_recipes_web/controllers/user_controller_test.exs
+++ test/my_recipes_web/controllers/user_controller_test.exs
@@ -1,56 +1,94 @@
 defmodule MyRecipesWeb.UserControllerTest do
   use MyRecipesWeb.ConnCase
-
-  alias MyRecipes
+  import MyRecipesWeb.Auth.Guardian
+  alias MyRecipes.User
 
   @create_attrs %{
     "username" => "test",
     "password" => "test123"
   }
 
-  def fixture(:user) do
-    {:ok, user} = MyRecipes.create_user(@create_attrs)
-    user
+  describe "show/2" do
+    setup %{conn: conn} do
+      params = @create_attrs
+      {:ok, user} = MyRecipes.create_user(params)
+      {:ok, token, _claims} = encode_and_sign(user)
+
+      conn = put_req_header(conn, "authorization", "Bearer #{token}")
+      {:ok, conn: conn}
+    end
+
+    test "when there is a user with the given id, returns the user", %{conn: conn} do
+      {:ok, %User{id: id}} = MyRecipes.create_user(@create_attrs)
+
+      response =
+        conn
+        |> get(Routes.user_path(conn, :show, id))
+        |> json_response(:ok)
+
+      %{"user" => user} = response
+
+      assert %{"id" => _id, "recipes" => _recipes, "username" => "test"} = user
+    end
   end
 
   describe "create" do
     test "renders user when data is valid", %{conn: conn} do
-      conn = post(conn, Routes.user_path(conn, :create), @create_attrs)
-      assert %{"id" => id, "password" => password, "recipes" => recipes} = json_response(conn, 201)
+      response =
+        conn
+        |> post(Routes.user_path(conn, :create, @create_attrs))
+        |> json_response(201)
 
-      conn = get(conn, Routes.user_path(conn, :show, id))
       assert %{
-        "id" => ^id,
-        "password" => ^password,
-        "username" => "test",
-        "recipes" => ^recipes,
-      } = json_response(conn, 200)
+               "message" => "User created!",
+               "token" => _token,
+               "user" => %{"id" => _id, "username" => "test"}
+             } = response
     end
   end
 
   describe "update" do
-    setup [:create_user]
+    setup %{conn: conn} do
+      params = @create_attrs
+      {:ok, user} = MyRecipes.create_user(params)
+      {:ok, token, _claims} = encode_and_sign(user)
 
-    test "updates chosen user", %{conn: conn, user: user} do
-      conn = put(conn, Routes.user_path(conn, :update, user), @create_attrs)
-      assert response(conn, 200)
+      conn = put_req_header(conn, "authorization", "Bearer #{token}")
+      {:ok, conn: conn}
+    end
+
+    test "updates chosen user", %{conn: conn} do
+      {:ok, %User{id: id}} = MyRecipes.create_user(@create_attrs)
+
+      response =
+        conn
+        |> put(Routes.user_path(conn, :update, id), @create_attrs)
+        |> json_response(:ok)
+
+      %{"user" => user} = response
+
+      assert %{"id" => _id, "recipes" => _recipes, "username" => "test"} = user
     end
   end
 
   describe "delete" do
-    setup [:create_user]
+    setup %{conn: conn} do
+      params = @create_attrs
+      {:ok, user} = MyRecipes.create_user(params)
+      {:ok, token, _claims} = encode_and_sign(user)
 
-    test "deletes chosen user", %{conn: conn, user: user} do
-      conn = delete(conn, Routes.user_path(conn, :delete, user))
-      assert response(conn, 200)
-
-      conn = get(conn, Routes.user_path(conn, :show, user))
-      assert %{"message" => "not_found"} = json_response(conn, 404)
+      conn = put_req_header(conn, "authorization", "Bearer #{token}")
+      {:ok, conn: conn}
     end
-  end
 
-  defp create_user(_) do
-    user = fixture(:user)
-    %{user: user}
+    test "deletes chosen user", %{conn: conn} do
+      {:ok, %User{id: id}} = MyRecipes.create_user(@create_attrs)
+
+      conn
+      |> delete(Routes.user_path(conn, :delete, id), @create_attrs)
+      |> json_response(:ok)
+
+      assert %{"message" => "User with id #{id} deleted!"}
+    end
   end
 end
